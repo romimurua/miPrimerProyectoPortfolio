3ee123c70ed53482b93d118d25ad420ae4f84c37
romimurua <romimurua.prensa@gmail.com>
2022-08-18 03:23:14 -0300

    se agrega el portfolio

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,5 +1,64 @@
-console.log ("HOLA JS")
+const menu = document.querySelector("ul.nav--menu")
+const icon = document.getElementById ("menu-icon")
 
-const titulo = document.querySelector("h1")
 
-console.log (titulo)
+icon.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+    menu.classList.toggle("active")
+}
+
+
+
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slider = document.querySelector('.container--areas')
+
+
+prev.addEventListener('click', ( ) => {
+    slider.scrollLeft -= 300
+}
+
+)
+
+next.addEventListener('click', ( ) => {
+    slider.scrollLeft += 300
+}
+
+)
+
+
+const visual = document.querySelector('.visual')
+const pasar = document.querySelector('.pasar')
+const cambiar = document.querySelector('.cambiar')
+const seleccionar = document.querySelector('.seleccionar')
+const deslizar = document.querySelector('.portfolio')
+
+
+visual.addEventListener('click', ( ) => {
+    deslizar.scrollLeft -= 1000
+}
+
+)
+
+
+pasar.addEventListener('click', ( ) => {
+    deslizar.scrollLeft += 1000
+}
+
+)
+
+cambiar.addEventListener('click', ( ) => {
+    deslizar.scrollLeft -= 300
+}
+
+)
+
+
+seleccionar.addEventListener('click', ( ) => {
+    deslizar.scrollLeft += 300
+}
+
+)
+
+
